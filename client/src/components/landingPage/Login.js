39d21f311c10d8983c315f0247d029f058eb77a7
Samyak Jain <samyakjain971@gmail.com>
2023-08-07 00:43:19 +0530

    favicon updated, metaconnect done

--- client/src/components/landingPage/Login.js
+++ client/src/components/landingPage/Login.js
@@ -16,6 +16,7 @@ export default function Login(props) {
   const [password, setPassword] = useState("");
   const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+  const [metaAccount, setMetaAccount] = useState(''); // meta mask account
 
   useEffect(() => {
     const auth = async () => {
@@ -34,7 +35,7 @@ export default function Login(props) {
     auth();
   });
 
-  const handlePatientLogin = async (healthID, password) => {
+  const handlePatientLogin = async (healthID, password, metaAccount) => {
     setLoading(true);
     console.log("Pressed Login")
     try {
@@ -90,7 +91,7 @@ export default function Login(props) {
     // }
   };
 
-  const handleDoctorAdminLogin = async (email, password, path) => {
+  const handleDoctorAdminLogin = async (email, password, metaAccount, path) => {
     setLoading(true);
     const res = await fetch(path, {
       method: "POST",
@@ -139,25 +140,31 @@ export default function Login(props) {
     e.preventDefault();
     switch (Toggle) {
       case "Patient":
-        handlePatientLogin(username, password);
+        handlePatientLogin(username, password, metaAccount);
         break;
       case "Doctor":
-        handleDoctorAdminLogin(username, password, "/login/doctor");
+        handleDoctorAdminLogin(username, password, metaAccount, "/login/doctor");
         break;
       case "Admin":
-        handleDoctorAdminLogin(username, password, "/login/admin");
+        handleDoctorAdminLogin(username, password, metaAccount, "/login/admin");
         break;
       default:
         break;
     }
   };
   
-  const [showAccount, setShowAccount] = useState('');
   const getAccount = async () => {
     try {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const account = accounts[0];
-      setShowAccount(account);
+      if(metaAccount != ''){
+        setMetaAccount('');
+        console.log("meta account removed");
+      }
+      else{
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        setMetaAccount(account);
+        console.log("meta account added");  
+      }
     } catch (err) {
       if (err.code === 4001) {
         // EIP-1193 userRejectedRequest error
@@ -269,6 +276,21 @@ export default function Login(props) {
         />
         <span className="text-sm text-red-500">{passwordError}</span>
 
+        <div className="pt-4">
+          <input
+            onClick={getAccount}
+              className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+              type="checkbox"
+              role="switch"
+              id="flexSwitchCheckDefault02"
+              required
+           />
+          <label
+              className="inline-block pl-[0.15rem] hover:cursor-pointer text-semibold"
+              htmlFor="flexSwitchCheckDefault"
+          >Connect to MetaMask Wallet</label>
+      </div>
+
         {Loading ? (
           <div className="flex justify-center items-center py-3">
             <ReactLoading
@@ -290,10 +312,6 @@ export default function Login(props) {
       <h1 className="font-poppins text-base pt-5">
         New User, <Link to="/Register">Register here</Link>
       </h1>
-
-      <button type="submit" onClick={getAccount}
-            className="text-lg mt-10  bg-primary py-1 px-3 rounded font-semibold font-poppins shadow-sm hover:bg-bgsecondary"
-        >Connect</button>
     </div>
   );
 }
