c079d06432b37bea8838552e13877872a01cd181
yash-pathak-1997 <yashhpathakk1997@gmail.com>
2023-08-21 00:00:24 +0530

    Updated contract

--- client/src/components/landingPage/contracts/PaymentManagement.sol
+++ client/src/components/landingPage/contracts/PaymentManagement.sol
@@ -1,567 +1,128 @@
-[
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "allClaim",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "id",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "sender",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "reciever",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "status",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "txHash",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "date",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "allTx",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "sender",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "reciever",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "txHash",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "date",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "status",
-				"type": "string"
-			}
-		],
-		"name": "getClaimsByStatusRecieve",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "string",
-						"name": "id",
-						"type": "string"
-					},
-					{
-						"internalType": "address",
-						"name": "sender",
-						"type": "address"
-					},
-					{
-						"internalType": "address",
-						"name": "reciever",
-						"type": "address"
-					},
-					{
-						"internalType": "uint256",
-						"name": "amount",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "status",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "txHash",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "date",
-						"type": "string"
-					}
-				],
-				"internalType": "struct PaymentManagement.ClaimRequest[]",
-				"name": "",
-				"type": "tuple[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "status",
-				"type": "string"
-			}
-		],
-		"name": "getClaimsByStatusSend",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "string",
-						"name": "id",
-						"type": "string"
-					},
-					{
-						"internalType": "address",
-						"name": "sender",
-						"type": "address"
-					},
-					{
-						"internalType": "address",
-						"name": "reciever",
-						"type": "address"
-					},
-					{
-						"internalType": "uint256",
-						"name": "amount",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "status",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "txHash",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "date",
-						"type": "string"
-					}
-				],
-				"internalType": "struct PaymentManagement.ClaimRequest[]",
-				"name": "",
-				"type": "tuple[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getRecievePayment",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "address",
-						"name": "sender",
-						"type": "address"
-					},
-					{
-						"internalType": "address",
-						"name": "reciever",
-						"type": "address"
-					},
-					{
-						"internalType": "uint256",
-						"name": "amount",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "txHash",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "date",
-						"type": "string"
-					}
-				],
-				"internalType": "struct PaymentManagement.Payment[]",
-				"name": "payments",
-				"type": "tuple[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getSendPayment",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "address",
-						"name": "sender",
-						"type": "address"
-					},
-					{
-						"internalType": "address",
-						"name": "reciever",
-						"type": "address"
-					},
-					{
-						"internalType": "uint256",
-						"name": "amount",
-						"type": "uint256"
-					},
-					{
-						"internalType": "string",
-						"name": "txHash",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "date",
-						"type": "string"
-					}
-				],
-				"internalType": "struct PaymentManagement.Payment[]",
-				"name": "payments",
-				"type": "tuple[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "recieveClaimMapping",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "id",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "sender",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "reciever",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "status",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "txHash",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "date",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "recieveTxAddMapping",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "sender",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "reciever",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "txHash",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "date",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "sendClaimMapping",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "id",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "sender",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "reciever",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "status",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "txHash",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "date",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "sendTxAddMapping",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "sender",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "reciever",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "txHash",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "date",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_id",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "_reciever",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "_status",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_txHash",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_date",
-				"type": "string"
-			}
-		],
-		"name": "storeClaim",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_reciever",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_amount",
-				"type": "uint256"
-			},
-			{
-				"internalType": "string",
-				"name": "_txHash",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "_date",
-				"type": "string"
-			}
-		],
-		"name": "storePayment",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "id",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "newStatus",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "_sender",
-				"type": "address"
-			}
-		],
-		"name": "updateClaimStatusById",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.6.0 <0.9.0;
+
+contract PaymentManagement {
+
+    // payment
+    struct Payment {
+        address sender;
+        address reciever;
+        uint amount;
+        string txHash;
+        string date;
+    }
+
+    struct ClaimRequest {
+        string id;
+        address sender;
+        address reciever;
+        uint amount;
+        string status;
+        string txHash;
+        string date;
+    }
+
+    mapping (address => Payment[]) public sendTxAddMapping;
+    mapping (address => Payment[]) public recieveTxAddMapping;
+    mapping (address => ClaimRequest[]) public sendClaimMapping;
+    mapping (address => ClaimRequest[]) public recieveClaimMapping;
+
+    Payment[] public allTx;
+    ClaimRequest[] public allClaim;
+
+    function storePayment(address  _reciever, uint _amount, string memory _txHash, string memory _date) external {
+        Payment memory payment = Payment({
+            sender: msg.sender,
+            reciever: _reciever,
+            amount: _amount,
+            txHash: _txHash,
+            date: _date
+        });
+
+        sendTxAddMapping[msg.sender].push(payment);
+        recieveTxAddMapping[payment.reciever].push(payment);
+    }
+
+    function getSendPayment() public view returns (Payment[] memory payments) {
+        return sendTxAddMapping[msg.sender];
+    }
+
+    function getRecievePayment() public view returns (Payment[] memory payments) {
+        return recieveTxAddMapping[msg.sender];
+    }
+
+    function storeClaim(string memory _id, address _reciever, uint _amount, string memory _status, string memory _txHash, string memory _date)  external {
+        ClaimRequest memory claimRequest = ClaimRequest({
+            id: _id,
+            sender: msg.sender,
+            reciever: _reciever,
+            amount: _amount,
+            status: _status,
+            txHash: _txHash,
+            date: _date
+        });
+
+        sendClaimMapping[msg.sender].push(claimRequest);
+        recieveClaimMapping[claimRequest.reciever].push(claimRequest);
+    }
+
+    function getClaimsByStatusRecieve(string memory status) public view returns (ClaimRequest[] memory) {
+        ClaimRequest[] storage allClaims = recieveClaimMapping[msg.sender];
+        uint count = allClaims.length;
+        ClaimRequest[] memory result = new ClaimRequest[](count);
+        uint resultCount = 0;
+
+        for (uint i = 0; i < count; i++) {
+            if (keccak256(bytes(allClaims[i].status)) == keccak256(bytes(status))) {
+                result[resultCount] = allClaims[i];
+                resultCount++;
+            }
+        }
+
+        assembly {
+            mstore(result, resultCount)
+        }
+
+        return result;
+    }
+
+    function getClaimsByStatusSend(string memory status) public view returns (ClaimRequest[] memory) {
+        ClaimRequest[] storage allClaims = sendClaimMapping[msg.sender];
+        uint count = allClaims.length;
+        ClaimRequest[] memory result = new ClaimRequest[](count);
+        uint resultCount = 0;
+
+        for (uint i = 0; i < count; i++) {
+            if (keccak256(bytes(allClaims[i].status)) == keccak256(bytes(status))) {
+                result[resultCount] = allClaims[i];
+                resultCount++;
+            }
+        }
+
+        assembly {
+            mstore(result, resultCount)
+        }
+
+        return result;
+    }
+
+    function updateClaimStatusById(string memory id, string memory newStatus, address _sender) external {
+        ClaimRequest[] storage recieveClaims = recieveClaimMapping[msg.sender];
+        ClaimRequest[] storage sendClaims = sendClaimMapping[_sender];
+
+        
+        for (uint i = 0; i < recieveClaims.length; i++) {
+            if (keccak256(bytes(recieveClaims[i].id)) == keccak256(bytes(id))) {
+                recieveClaims[i].status = newStatus;
+            }
+        }
+
+        for (uint i = 0; i < sendClaims.length; i++) {
+            if (keccak256(bytes(sendClaims[i].id)) == keccak256(bytes(id))) {
+                sendClaims[i].status = newStatus;
+            }
+        }
+    }
+
+
+}
